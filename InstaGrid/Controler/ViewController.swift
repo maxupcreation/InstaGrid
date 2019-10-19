ad37375447d30d2a10cd1d0e4c3ff93d057a7fd9
Maxime <maxupcreation@gmail.com>
2019-10-19 09:49:56 +0200

    fix

--- InstaGrid/Controler/ViewController.swift
+++ InstaGrid/Controler/ViewController.swift
@@ -9,44 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-    @IBOutlet weak var buttonPicture: UIImageView!
-    @IBOutlet weak var ButtonPicture2: UIButton!
-    @IBOutlet weak var ButtonPicture3: UIButton!
-    @IBOutlet weak var ButtonPicture4: UIButton!
+    
+
+    @IBOutlet weak var buttonPicture1: UIImageView!
+    @IBOutlet weak var buttonPicture2: UIImageView!
+    @IBOutlet weak var buttonPicture3: UIImageView!
+    @IBOutlet weak var buttonPicture4: UIImageView!
     
     @IBAction func addPicture(_ sender: AnyObject) {
            let image = UIImagePickerController()
            image.delegate = self
-           image.sourceType = UIImagePickerController.SourceType.photoLibrary
+           image.sourceType = .photoLibrary
            image.allowsEditing = true
            self.present (image,animated: true )
-           
-           func imagePickerController(_ picker: UIImagePickerController,
-           didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
-               if  let image = info[.originalImage] as? UIImage {
-                   buttonPicture.image = image
-               }
-               self.dismiss(animated: true, completion: nil)
            }
-       
-       }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+    didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
+      buttonPicture1.image = info [.editedImage] as? UIImage
+       dismiss(animated : true, completion : nil)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func ButtonLayout2(_ sender: Any) {
-        ButtonPicture3.isHidden = true
-          ButtonPicture2.isHidden = false
-    }
     @IBAction func ButtonLayout3(_ sender: Any) {
-        ButtonPicture2.isHidden = true
-        ButtonPicture3.isHidden = false
+           buttonPicture2.isHidden = true
+           buttonPicture3.isHidden = false
     }
+   
+    @IBAction func ButtonLayout2(_ sender: Any) {
+        buttonPicture3.isHidden = true
+          buttonPicture2.isHidden = false
+    }
+   
+    
     @IBAction func ButtonLayout1(_ sender: Any) {
-       ButtonPicture2.isHidden = false
-        ButtonPicture3.isHidden = false
+       buttonPicture2.isHidden = false
+        buttonPicture3.isHidden = false
     }
     
    
