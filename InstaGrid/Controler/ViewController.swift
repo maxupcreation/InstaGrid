f0de3a5a1d9f400abc7bb9403eec2446e81d6a68
Maxime <maxupcreation@gmail.com>
2019-10-31 10:59:56 +0100

    add animate swipe

--- InstaGrid/Controler/ViewController.swift
+++ InstaGrid/Controler/ViewController.swift
@@ -13,64 +13,84 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     var currentButton : UIButton?
     
     @IBOutlet var buttonPicture: [UIButton]!
-    @IBOutlet weak var SquarrePictureLayout: UIView!
+    @IBOutlet weak var squarrePictureLayout: UIView!
     
     @IBAction func addPicture(_ sender: UIButton) {
         currentButton = sender
-           let imagePicker = UIImagePickerController()
-           imagePicker.delegate = self
-           imagePicker.sourceType = .photoLibrary
-           imagePicker.allowsEditing = false
-           present (imagePicker,animated: true, completion: nil )
-           }
-
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
+        present (imagePicker,animated: true, completion: nil )
+    }
+    
     
     func imagePickerController(_ picker: UIImagePickerController,  didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
         
         picker.dismiss(animated: true, completion: nil)
-    if let image = info[.originalImage] as? UIImage {
-        currentButton!.setImage(image, for: .normal)
-        currentButton!.imageView?.contentMode = .scaleAspectFill
-    
-        
+        if let image = info[.originalImage] as? UIImage {
+            currentButton!.setImage(image, for: .normal)
+            currentButton!.imageView?.contentMode = .scaleAspectFill
+            
+            
             
         }
     }
     
-
-   
+    
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        let swipeGesture = UISwipeGestureRecognizer(target: self, action:#selector(SwipeAction(_:)))
+        swipeGesture.direction = .up
+        self.view.addGestureRecognizer(swipeGesture)
     }
-  
+    
+    
     
     @IBAction func buttonLayOut(_ sender: Any) {
         buttonPicture[1].isHidden = true
-         buttonPicture[3].isHidden = false
-   
+        buttonPicture[3].isHidden = false
+        
         
     }
     
     @IBAction func buttonLayOut2(_ sender: Any) {
-         buttonPicture[3].isHidden = true
+        buttonPicture[3].isHidden = true
         buttonPicture[1].isHidden = false
     }
     
     @IBAction func buttonLayout3(_ sender: Any) {
-         buttonPicture[1].isHidden = false
+        buttonPicture[1].isHidden = false
         buttonPicture[3].isHidden = false
     }
     
-    @IBAction func swipeForShare(sender: UISwipeGestureRecognizer) {
-      
-          
+    
+    
+    @IBAction func SwipeAction(_ sender: UISwipeGestureRecognizer) {
+        UIView.animate(withDuration: 1) {
+            self.squarrePictureLayout.transform = CGAffineTransform(translationX: 0, y: -200)
+            self.squarrePictureLayout.alpha = 0
+            
+            
+            
+        }
+        
+        func sharePhoto(sender : UIActivityViewController){
+            let activityViewController = UIActivityViewController(activityItems: [currentButton!], applicationActivities: nil)
+            
+            present(activityViewController, animated: true, completion: nil)
         }
         
     }
     
-
     
+    
+    
+}
+
+
+
 
